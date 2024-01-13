8fd5213d1fe93c4f576ba8b293e5c1c7d38e892b
Fayzillo Khamraev <absolemgen@gmail.com>
2024-01-13 19:06:35 +0100

    hmw 6

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+class Storage {
+  constructor(item) {
+    this.items = item;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    const index = this.items.indexOf(item);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+  }
+}
+// const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem('Droid');
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem('Prolonger');
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
